b69e50a6ec0750026e38f08d4b95b68bb58f1b0f
Dubravka Drčelić <duca.drcelic25@gmail.com>
2024-05-18 23:45:15 +0200

    added table recipes

--- app.py
+++ app.py
@@ -2,9 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+def create_table(connection):
+    cursor = connection.cursor()
+    query = """
+        CREATE TABLE IF NOT EXISTS recipes 
+        (
+            id INTEGER PRIMARY KEY, 
+            title varchar(255) NOT NULL, 
+            category varchar(255) CHECK (category IN ('lunch','snack','dessert','dinner','breakfast')), 
+            servings INTEGER NOT NULL, 
+            ingredients varchar(255) NOT NULL, 
+            directions varchar(255) NOT NULL
+        );
+    """
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
