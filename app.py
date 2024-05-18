434c42ccbcc71fd2bd5ae6654548f201dd05170a
Dubravka Drčelić <duca.drcelic25@gmail.com>
2024-05-19 00:01:56 +0200

    added routes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 
+# Creats table recipes if not exist in db
+# Has fields id, title, category, servings, ingredients, directions
 def create_table(connection):
     cursor = connection.cursor()
     query = """
@@ -21,9 +23,34 @@ def create_table(connection):
     cursor.close()
 
 
-@app.route("/")
+# Shows main page -> index
+# Uses http method GET
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+
+@app.rout("/add", methods=["POST"])
+def add():
+    pass
+
+@app.rout("/get", methods=["GET"])
+def get():
+    pass
+
+@app.rout("/get/<int:id>", methods=["GET"])
+def get_recipe(id):
+    pass
+
+
+@app.rout("/update/<int:id>", methods=["PUT"])
+def update(id):
+    pass
+
+
+@app.rout("/delete/<int:id>", methods=["DELETE"])
+def delete(id):
+    pass
 
 
 if __name__ == "__main__":
